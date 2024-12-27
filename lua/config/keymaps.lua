9d19ee5fceb312b144c4d11e4c3d2440b9328db7
Lyrics2196 <linyr2196@outlook.com>
2024-12-27 13:32:35 +0800

    change key map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,11 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.g.mapleader = " "
-
 local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>")
-
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")

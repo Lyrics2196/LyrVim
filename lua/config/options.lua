b100a9f96cf5b3882aa4bfda61407eef0a282ee9
Lyrics <linyr2196@outlook.com>
2025-02-22 14:51:04 +0800

    new

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -36,3 +36,10 @@ opt.shellquote = '"'
 opt.shellxquote = ""
 
 opt.conceallevel = 0
+
+opt.spell = false
+
+vim.g.neovide_title_background_color =
+  string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
+
+vim.g.neovide_title_text_color = "white"

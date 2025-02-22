088f0a51dde21599b904a0cf2ebbcbcef4900913
Lyrics <linyr2196@outlook.com>
2025-02-22 21:46:14 +0800

    Disable spell checking and auto-complete for markdown file.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,5 +4,10 @@ return {
     keymap = {
       preset = "super-tab",
     },
+    enabled = function()
+      return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
+        and vim.bo.buftype ~= "prompt"
+        and vim.b.completion ~= false
+    end,
   },
 }
